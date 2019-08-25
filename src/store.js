65e97ce4bd76e4a1c04791bff42e084e35d65bcc
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-25 19:06:01 +0300

    Define store: initial state, mutations, actions, getters

--- src/store.js
+++ src/store.js
@@ -1,10 +1,46 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import Note from "@/models/Note";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    notes: [],
+    selectedNoteId: null
+  },
+  mutations: {
+    addNote: state => {
+      const note = Note.create(`New note`, `### Content of a new note`);
+
+      state.notes.push(note);
+    },
+    removeNote: (state, index) => {
+      state.notes.splice(index, 1);
+    },
+    selectNoteId: (state, id) => {
+      state.selectedNoteId = id;
+    }
+  },
+  actions: {
+    removeNote: ({ getters, commit }, id) => {
+      const index = getters.getNoteIndexById(id);
+
+      index !== -1 && commit("removeNote", index);
+    }
+  },
+  getters: {
+    selectedNote: ({ notes, selectedNoteId }) => {
+      return notes.find(note => note.id === selectedNoteId);
+    },
+    numberOfNotes: ({ notes }) => {
+      return notes.length;
+    },
+    isNoteSelected: ({ selectedNoteId }) => id => {
+      return selectedNoteId === id;
+    },
+    getNoteIndexById: ({ notes }) => id => {
+      return notes.findIndex(note => note.id === id);
+    }
+  }
 });
