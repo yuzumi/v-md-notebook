2828c76629a5f3b65754aaf9d4a8e03b8d9650ca
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-25 10:45:18 +0300

    Apply created components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,52 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <aside class="section sidebar bg-light"></aside>
+    <main class="section main">
+      <Editor :value="content" @setValue="setContent" />
+      <StatusBar :content="content" />
+    </main>
+    <aside class="section preview bg-light">
+      <Previewer :markdown="content" />
+    </aside>
+  </div>
 </template>
 
 <script>
+import Editor from "@/components/Editor";
+import Previewer from "@/components/Previewer";
+import StatusBar from "@/components/StatusBar";
+
 export default {
-  name: "app"
+  name: "app",
+  data() {
+    return {
+      content: ""
+    };
+  },
+  methods: {
+    setContent(value) {
+      this.content = value;
+    }
+  },
+  components: {
+    Editor,
+    Previewer,
+    StatusBar
+  }
 };
 </script>
 
-<style></style>
+<style>
+@import "~bootstrap-material-design/dist/css/bootstrap-material-design.min.css";
+@import "assets/styles/index.css";
+
+.section {
+  height: 100%;
+  padding: 1rem;
+}
+
+.main {
+  display: flex;
+  flex-direction: column;
+}
+</style>
