22521c25c1a47a0b2ef8900ec7b02072f01da4f4
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-25 10:43:31 +0300

    Create the Previewer component

---
+++ src/components/Previewer.vue
@@ -0,0 +1,22 @@
+<template>
+  <p class="previewer mb-0" v-html="compiledMarkdown"></p>
+</template>
+
+<script>
+import marked from "marked";
+
+export default {
+  name: "previewer",
+  props: {
+    markdown: {
+      type: String,
+      default: ""
+    }
+  },
+  computed: {
+    compiledMarkdown() {
+      return marked(this.markdown);
+    }
+  }
+};
+</script>
