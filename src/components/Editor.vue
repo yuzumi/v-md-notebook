576ae555f7dc1befac6421320e1663698d59335f
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-25 10:42:11 +0300

    Create the Editor component

---
+++ src/components/Editor.vue
@@ -0,0 +1,33 @@
+<template>
+  <form class="editor mb-0">
+    <textarea
+      class="form-control editor__workspace"
+      :value="value"
+      @input="$emit('setValue', $event.target.value)"
+    ></textarea>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "editor",
+  props: {
+    value: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style>
+.editor {
+  flex: 1 1;
+}
+
+.editor__workspace {
+  height: 100%;
+  background-image: none !important;
+  resize: none;
+}
+</style>
