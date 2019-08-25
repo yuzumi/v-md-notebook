a9a9cb1fb4400b77d9b571a1620d3ff4a985d418
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-25 12:39:24 +0300

    Install @fontawesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
 
+import "@fortawesome/fontawesome-free/js/all.min.js";
+
 Vue.config.productionTip = false;
 
 new Vue({
