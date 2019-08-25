ef8accf3ba44e5af881d02be0216be8003e7ba62
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-25 12:40:37 +0300

    Create the Toolbar component

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="toolbar">
+    <div class="d-flex align-items-center">
+      <form class="mb-0" style="flex: 1 1">
+        <div class="form-group mb-0">
+          <input class="form-control" type="text" v-model="note.title" />
+        </div>
+      </form>
+      <button
+        class="btn btn-danger btn-sm ml-2"
+        @click="$emit('remove', note.id)"
+      >
+        <i class="fas fa-trash"></i>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "toolbar",
+  props: {
+    note: {
+      type: Object,
+      default() {
+        return {};
+      }
+    }
+  }
+};
+</script>
