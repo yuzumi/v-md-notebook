553fc8a9b2f41e2c98f72e6048ab5d119dcb20eb
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-25 12:45:48 +0300

    Expand app features

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <template>
   <div id="app">
-    <aside class="section sidebar bg-light"></aside>
-    <main class="section main">
-      <Editor :value="content" @setValue="setContent" />
-      <StatusBar :content="content" />
-    </main>
-    <aside class="section preview bg-light">
-      <Previewer :markdown="content" />
+    <aside class="section sidebar bg-light">
+      <button
+        class="btn btn-outline-primary btn-sm btn-block"
+        type="button"
+        @click="addNote"
+      >
+        Add note ({{ numberOfNotes }})
+      </button>
+      <NoteList
+        :notes="notes"
+        :selectedNoteId="selectedNoteId"
+        @selectNote="selectNoteId"
+      />
     </aside>
+    <template v-if="selectedNote">
+      <main class="section main">
+        <Toolbar :note="selectedNote" @remove="removeNote" />
+        <Editor :note="selectedNote" />
+        <StatusBar :content="selectedNote.content" />
+      </main>
+      <aside class="section preview bg-light">
+        <Previewer :markdown="selectedNote.content" />
+      </aside>
+    </template>
   </div>
 </template>
 
@@ -15,28 +31,70 @@
 import Editor from "@/components/Editor";
 import Previewer from "@/components/Previewer";
 import StatusBar from "@/components/StatusBar";
+import NoteList from "@/components/NoteList";
+import Toolbar from "@/components/Toolbar";
+
+import Note from "@/models/Note";
 
 export default {
   name: "app",
   data() {
     return {
-      content: ""
+      notes: [],
+      selectedNoteId: null
     };
   },
   methods: {
     setContent(value) {
       this.content = value;
+    },
+    addNote() {
+      const note = Note.create(
+        `New note #${this.newNoteNumber}`,
+        `### Content of a new note`
+      );
+
+      this.notes.push(note);
+    },
+    removeNote(id) {
+      const index = this.findNoteIndexById(id);
+
+      index !== -1 && this.notes.splice(index, 1);
+    },
+    selectNoteId(id) {
+      this.selectedNoteId = id;
+    },
+    findNoteIndexById(id) {
+      return this.notes.findIndex(note => note.id === id);
+    }
+  },
+  computed: {
+    numberOfNotes() {
+      return this.notes.length;
+    },
+    newNoteNumber() {
+      return this.numberOfNotes + 1;
+    },
+    selectedNote() {
+      if (this.selectedNoteId) {
+        return this.notes.find(note => note.id === this.selectedNoteId);
+      }
+
+      return null;
     }
   },
   components: {
     Editor,
     Previewer,
-    StatusBar
+    StatusBar,
+    NoteList,
+    Toolbar
   }
 };
 </script>
 
 <style>
+@import "~@fortawesome/fontawesome-free/css/all.min.css";
 @import "~bootstrap-material-design/dist/css/bootstrap-material-design.min.css";
 @import "assets/styles/index.css";
 
@@ -48,5 +106,9 @@ export default {
 .main {
   display: flex;
   flex-direction: column;
+}
+
+.sidebar {
+  overflow-y: auto;
 }
 </style>
